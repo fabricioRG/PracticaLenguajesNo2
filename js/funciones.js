dd167c29406fff7b269a045fbfd461a7e02baba1
Fabricio R <fabricio21rg@gmail.com>
2018-08-24 03:50:22 -0600

    Ya se ha logrado leer el archivo en un div editable

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,22 @@
 var openFile = function(event) {
-        var input = event.target;
+  var input = event.target;
+  var reader = new FileReader();
+  reader.onload = function(){
+    var text = reader.result;
+    var node = document.getElementById('divEditable');
+    node.innerText = text;
+    console.log(reader.result.substring(0, 200));
+  };
+  reader.readAsText(input.files[0]);
+};
 
-        var reader = new FileReader();
-        reader.onload = function(){
-          var text = reader.result;
-          var node = document.getElementById('cargaTexto');
-          node.innerText = text;
-          console.log(reader.result.substring(0, 200));
-        };
-        reader.readAsText(input.files[0]);
-      };
+function analizarTexto(id){
+  var texto = document.getElementById(id);
+  var strAnalizar = texto.innerText;
+  if(strAnalizar != ""){
+    alert(strAnalizar);
+  } else {
+    alert("No hay contenido cargado");
+  }
+}
+
